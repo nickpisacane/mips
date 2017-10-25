a3da9ca67f480995d1f85e9e2c0865d5589eda13
Nicholas Pisacane <pisacanen@gmail.com>
2017-10-25 16:59:22 -0700

    Move register mapping to utils

--- src/Assembler/Assembler.ts
+++ src/Assembler/Assembler.ts
@@ -6,46 +6,13 @@ import Instruction, {
   IInstruction,
   JInstruction,
 } from '../Instruction'
+import {
+  registers,
+  instructions,
+} from '../utils/mappings'
 
 const BASE_TEXT_ADDR = 0x00400000
 const BASE_DATA_ADDR = 0x10010000
-
-const registers: {
-  [key: string]: number
-} = {
-  '$0': 0,
-  '$at': 1,
-  '$v0': 2,
-  '$v1': 3,
-  '$a0': 4,
-  '$a1': 5,
-  '$a2': 6,
-  '$a3': 7,
-  '$t0': 8,
-  '$t1': 9,
-  '$t2': 10,
-  '$t3': 11,
-  '$t4': 12,
-  '$t5': 13,
-  '$t6': 14,
-  '$t7': 15,
-  '$s0': 16,
-  '$s1': 17,
-  '$s2': 18,
-  '$s3': 19,
-  '$s4': 20,
-  '$s5': 21,
-  '$s6': 22,
-  '$s7': 23,
-  '$t8': 24,
-  '$t9': 25,
-  '$k0': 26,
-  '$k1': 27,
-  '$gp': 28,
-  '$sp': 29,
-  '$fp': 30,
-  '$ra': 31,
-}
 
 export default class Assembler {
   private astRoot: AST.Root
