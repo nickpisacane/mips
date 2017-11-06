0b01e4f11d67b0fe7a4747bac313eadbf8ccac2d
Nick Pisacane <pisacanen@gmail.com>
2017-11-05 16:43:12 -0800

    add code reducer

--- demo/src/reducers/index.ts
+++ demo/src/reducers/index.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux'
 
 import console, { ConsoleState } from './console'
+import code, { CodeState } from './code'
 
 export interface State {
   console: ConsoleState
+  code: CodeState
 }
 
 export default combineReducers<State>({
   console,
+  code,
 })
