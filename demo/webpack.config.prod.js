bec361fbfb314f9f31e2998bba29bf36500e55da
Nick Pisacane <pisacanen@gmail.com>
2017-11-05 19:13:36 -0800

    prod config

---
+++ demo/webpack.config.prod.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+
+// TODO: Optimize Prod Build
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './index.tsx',
+  output: {
+    path: path.normalize(path.join(__dirname, '..', 'docs', 'js')),
+    filename: 'app.js',
+    publicPath: '/js/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false,
+      },
+    })
+  ],
+}
