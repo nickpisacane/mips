d617a5447eec3a0c1109f9e41a8eed8c5d2b2a27
Nick Pisacane <pisacanen@gmail.com>
2017-11-05 19:06:11 -0800

    Add clear button and fix newline issue

--- demo/src/components/Console/index.tsx
+++ demo/src/components/Console/index.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react'
+import { Card, CardTitle } from 'material-ui/Card'
 import styled from 'styled-components'
+import { Toolbar, ToolbarGroup, ToolbarTitle, ToolbarSeparator } from 'material-ui/Toolbar'
+import RaisedButton from 'material-ui/RaisedButton'
 
 const Container = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: flex-start;
   flex-direction: column;
+  height: 100%;
 `
 
 const ConsoleTextarea = styled.textarea`
   font-family: 'Roboto Mono';
-  flex-grow: 1;
+  flex: 1;
+  display: block;
   width: 100%;
+  box-sizing: border-box;
+  resize: none;
+`
+
+const StyledCard = styled(Card)`
+  height: 100%;
+  & > div {
+    height: 100%;
+  }
 `
 
 export interface Props {
@@ -21,11 +35,25 @@ export interface Props {
 }
 
 export default class Console extends React.Component<Props> {
+  private textarea: HTMLTextAreaElement
+
+  private textareaRef = (node: HTMLTextAreaElement | null) => {
+    if (node) {
+      this.textarea = node
+    }
+  }
+
   private handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { data } = this.props
     const { value } = event.target
     const delta = value.substr(data.length)
     this.props.onData(delta)
+  }
+
+  public focus() {
+    if (this.textarea) {
+      this.textarea.focus()
+    }
   }
 
   render() {
@@ -37,7 +65,24 @@ export default class Console extends React.Component<Props> {
 
     return (
       <Container {...rest}>
-        <ConsoleTextarea value={data} onChange={this.handleTextareaChange}>
+        <Toolbar style={{width: '100%'}}>
+          <ToolbarGroup firstChild style={{paddingLeft: '12px'}}>
+            <ToolbarTitle text='Console' />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ToolbarSeparator />
+            <RaisedButton
+              primary
+              label='Clear'
+              onTouchTap={onClear}
+            />
+          </ToolbarGroup>
+        </Toolbar>
+        <ConsoleTextarea
+          value={data.replace(/\\n/g, '\n')}
+          onChange={this.handleTextareaChange}
+          innerRef={this.textareaRef}
+        >
         </ConsoleTextarea>
       </Container>
     )
