c59939fa12856a8c5ef6e8b9edbba37c315843f7
Nick Pisacane <pisacanen@gmail.com>
2017-11-05 16:42:38 -0800

    State connected console

---
+++ demo/src/containers/ConnectedConsole.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { bindActionCreators, Dispatch } from 'redux'
+import { connect } from 'react-redux'
+
+import { State } from '../reducers'
+import { ConsoleState, clear } from '../reducers/console'
+import Console, { Props } from '../components/Console'
+
+const mapStateToProps = (state: State): Partial<Props> => ({
+  data: state.console.data,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch<State>): Partial<Props> =>
+  bindActionCreators({
+    onClear: clear,
+  }, dispatch)
+
+export default connect<
+  Partial<Props>,
+  Partial<Props>,
+  {
+    onData: (data: string) => void
+  }
+>(mapStateToProps, mapDispatchToProps)(Console)
