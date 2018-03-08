85aa8168932123113c0b44df0b8f0f7bcedd2e05
Nick Pisacane <pisacanen@gmail.com>
2018-03-08 13:40:21 -0800

    Better testing for Binary

--- test/utils/Binary.test.ts
+++ test/utils/Binary.test.ts
@@ -9,16 +9,27 @@ describe('Binary', () => {
   })
 
   it('setByte()', () => {
-    const binary = new Binary(8)
+    const binary = new Binary(16)
     binary.setByte(0, 8)
+    // 00000000 00001000
+    binary.setByte(1, 9)
+    // 00001001 00001000
+
     expect(binary.getBit(3)).to.equal(1)
+
+    expect(binary.getBit(8)).to.equal(1)
+    expect(binary.getBit(11)).to.equal(1)
   })
 
   it('getByte()', () => {
-    const binary = new Binary(8)
+    const binary = new Binary(16)
     binary.setBit(0, 1)
     binary.setBit(3, 1)
+    binary.setBit(8, 1)
+    // => 00000001 00001001
+
     expect(binary.getByte(0)).to.equal(9)
+    expect(binary.getByte(1)).to.equal(1)
   })
 
   it('setBit()', () => {
@@ -50,5 +61,65 @@ describe('Binary', () => {
     expect(binary.getValue()).to.equal(14)
   })
 
-  // TODO: More tests
+  it('fromNumber()', () => {
+    let binary = Binary.fromNumber(42)
+    expect(binary.size).to.equal(8)
+    expect(binary.toString()).to.equal('101010')
+    expect(binary.getValue()).to.equal(42)
+
+    binary = Binary.fromNumber(Math.pow(2, 32) - 1)
+    expect(binary.size).to.equal(32)
+    expect(binary.toString()).to.equal(
+      '11111111111111111111111111111111'
+    )
+    expect(binary.getValue()).to.equal(Math.pow(2, 32) - 1)
+  })
+
+  it('toString() returns the string value binary number', () => {
+    const binary = Binary.fromArray([1, 0, 1, 0, 1, 0])
+    expect(binary.toString()).to.equal('101010')
+    expect(binary.toString(16)).to.equal('2a')
+  })
+
+  it('toUint8Array()', () => {
+    const binary = Binary.fromNumber(258)
+    // 00000001 000000010
+    //     0        1
+    const arr = binary.toUint8Array()
+    expect(arr[0]).to.equal(1)
+    expect(arr[1]).to.equal(2)
+  })
+
+  it('toUint32Array()', () => {
+    const binary = Binary.fromNumber(Math.pow(2, 32) + 2)
+    // 00000000 00000000 00000000 00000001 | 00000000 00000000 00000000 00000010
+    //                  0                                     1
+    const arr = binary.toUint32Array()
+    expect(arr[0]).to.equal(1)
+    expect(arr[1]).to.equal(2)
+  })
+
+  it('fromUint8Array()', () => {
+    const arr = new Uint8Array(2)
+    arr[0] = 1
+    arr[1] = 42
+    // 00000001 00101010
+    //    0        1
+    const binary = Binary.fromUint8Array(arr)
+    expect(binary.getValue()).to.equal(256 + 42)
+    expect(binary.getByte(0)).to.equal(42)
+    expect(binary.getByte(1)).to.equal(1)
+  })
+
+  it('fromUint32Array()', () => {
+    const arr = new Uint32Array(2)
+    arr[0] = 1
+    arr[1] = 2
+    // 00000000 00000000 00000000 00000001 | 00000000 00000000 00000000 00000010
+    //                  0                                     1
+    const binary = Binary.fromUint32Array(arr)
+    expect(binary.getValue()).to.equal(Math.pow(2, 32) + 2)
+    expect(binary.getByte(0)).to.equal(2)
+    expect(binary.getByte(4)).to.equal(1)
+  })
 })
